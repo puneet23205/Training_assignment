e99cb4a2946c109e34cba245cb62a66fb2dc91d3
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-02-06 15:53:20 +0530

    Time: 13 ms (75.93%), Space: 46.3 MB (96.75%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,44 +1,76 @@
+// class Solution {
+//     public String frequencySort(String s) {
+
+//         // Step 1: Frequency Map
+//         HashMap<Character, Integer> map = new HashMap<>();
+
+//         for (char c : s.toCharArray()) {
+//             map.put(c, map.getOrDefault(c, 0) + 1);
+//         }
+
+//         // Step 2: Buckets
+//         int n = s.length();
+//         List<Character>[] bucket = new List[n + 1];
+
+//         for (char c : map.keySet()) {
+
+//             int freq = map.get(c);
+
+//             if (bucket[freq] == null) {
+//                 bucket[freq] = new ArrayList<>();
+//             }
+
+//             bucket[freq].add(c);
+//         }
+
+//         // Step 3: Build result
+//         StringBuilder sb = new StringBuilder();
+
+//         for (int i = n; i >= 1; i--) {
+
+//             if (bucket[i] != null) {
+
+//                 for (char c : bucket[i]) {
+
+//                     for (int j = 0; j < i; j++) {
+//                         sb.append(c);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return sb.toString();
+//     }
+// }
 class Solution {
     public String frequencySort(String s) {
 
-        // Step 1: Frequency Map
         HashMap<Character, Integer> map = new HashMap<>();
 
+        // Count frequency
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        // Step 2: Buckets
-        int n = s.length();
-        List<Character>[] bucket = new List[n + 1];
+        List<Character> list = new ArrayList<>(map.keySet());
 
-        for (char c : map.keySet()) {
+        // Sort by frequency
+        Collections.sort(list, (a, b) ->
+            map.get(b) - map.get(a)
+        );
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : list) {
 
             int freq = map.get(c);
 
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
-            }
-
-            bucket[freq].add(c);
-        }
-
-        // Step 3: Build result
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = n; i >= 1; i--) {
-
-            if (bucket[i] != null) {
-
-                for (char c : bucket[i]) {
-
-                    for (int j = 0; j < i; j++) {
-                        sb.append(c);
-                    }
-                }
+            for (int i = 0; i < freq; i++) {
+                sb.append(c);
             }
         }
 
         return sb.toString();
     }
 }
+
