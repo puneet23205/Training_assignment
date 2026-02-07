917f30da435815dcb5338f3d1b8e8929dbe04c7b
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-02-07 14:33:44 +0530

    Time: 2 ms (99.58%), Space: 61.9 MB (28.73%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,36 +1,54 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
 
-        int n = cardPoints.length;
+        // int n = cardPoints.length;
 
-        // Sum of all cards
-        int total = 0;
-        for (int x : cardPoints) {
-            total += x;
+        // // Sum of all cards
+        // int total = 0;
+        // for (int x : cardPoints) {
+        //     total += x;
+        // }
+
+        // // If we take all cards
+        // if (k == n) return total;
+
+        // int window = n - k;
+
+        // // Initial window sum
+        // int curr = 0;
+        // for (int i = 0; i < window; i++) {
+        //     curr += cardPoints[i];
+        // }
+
+        // int minSum = curr;
+
+        // // Sliding window
+        // for (int i = window; i < n; i++) {
+
+        //     curr += cardPoints[i];
+        //     curr -= cardPoints[i - window];
+
+        //     minSum = Math.min(minSum, curr);
+        // }
+
+        // return total - minSum;
+
+
+        //another method can be just by using variables 
+
+        int left=0,right=cardPoints.length-1;
+        int lsum=0,maxsum=0,rsum=0;
+
+        for( int i=0;i<k;i++){
+            lsum+=cardPoints[i];
+            maxsum=lsum;
         }
-
-        // If we take all cards
-        if (k == n) return total;
-
-        int window = n - k;
-
-        // Initial window sum
-        int curr = 0;
-        for (int i = 0; i < window; i++) {
-            curr += cardPoints[i];
+        for(int j=k-1;j>=0;j--){
+            lsum=lsum-cardPoints[j];
+            rsum=rsum+cardPoints[right];
+            right--;
+            maxsum=Math.max(maxsum,lsum+rsum);
         }
-
-        int minSum = curr;
-
-        // Sliding window
-        for (int i = window; i < n; i++) {
-
-            curr += cardPoints[i];
-            curr -= cardPoints[i - window];
-
-            minSum = Math.min(minSum, curr);
-        }
-
-        return total - minSum;
+        return maxsum;
     }
 }
