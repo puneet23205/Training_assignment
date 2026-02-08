4b918518a023836c828375c286d773817d018764
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-02-09 05:12:05 +0530

    Time: 0 ms (100%), Space: 48 MB (63.5%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        
+          long nn =n;
+        double ans=1.0;
+        if(nn<0) nn=-1*nn;
+        while(nn>=1){
+            if(nn % 2==1){
+                ans=ans*x;
+                nn= nn-1;
+            }
+            else{
+                x= x*x;
+                nn=nn/2;
+            }
+        }
+        if(n<0) {
+             ans= (1.0)/ans;
+        }
+        return ans;
+    }
+}
