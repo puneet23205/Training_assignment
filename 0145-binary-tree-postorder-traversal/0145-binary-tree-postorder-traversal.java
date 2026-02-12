9451e8f643357674d4e33689f703718b82ef049f
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-02-12 11:50:21 +0530

    Time: 1 ms (14.08%), Space: 43.1 MB (84.67%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,26 +13,57 @@
  *     }
  * }
  */
-class Solution {
-List<Integer> ans= new ArrayList<>();
+// class Solution {
+// List<Integer> ans= new ArrayList<>();
 
+//     public List<Integer> postorderTraversal(TreeNode root) {
+        
+//         postorder(root);
+
+//         return ans;
+//     }
+
+//     private void postorder( TreeNode node){
+
+//          if(node == null ) return;
+//         //traverse to extreme left
+//         postorder(node.left);
+
+//         //traverse to the extreme right of that subtree 
+//         postorder( node.right);
+
+//         // print the node value
+//         ans.add(node.val);
+//     }
+// }
+        //Traversal using two stack
+ 
+class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         
-        postorder(root);
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
+        
+        if(root==null) return ans;
+        st1.add(root);
 
+        while(!st1.isEmpty()){
+
+             TreeNode curr = st1.pop();
+
+            st2.add(curr.val);
+
+            if(curr.left!= null){
+                st1.add(curr.left);
+            }
+            if(curr.right!=null){
+                st1.add(curr.right);
+            }
+        }
+         while(!st2.isEmpty()){
+            ans.add(st2.pop());
+        }
         return ans;
     }
-
-    private void postorder( TreeNode node){
-
-         if(node == null ) return;
-        //traverse to extreme left
-        postorder(node.left);
-
-        //traverse to the extreme right of that subtree 
-        postorder( node.right);
-
-        // print the node value
-        ans.add(node.val);
-    }
-}
+ }
