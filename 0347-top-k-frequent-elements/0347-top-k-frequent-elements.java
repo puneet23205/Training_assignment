af25da6ae2a71ff258792006f6c78f871ccc8303
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-02-20 04:18:00 +0530

    Time: 61 ms (5.16%), Space: 48.7 MB (11.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,57 +1,37 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Store: number -> how many times it appears
-HashMap<Integer, Integer> map = new HashMap<>();
+       
+       HashMap<Integer,Integer> map = new HashMap<>();
 
-// Count frequency of each number
-for (int num : nums) {
-    map.put(num, map.getOrDefault(num, 0) + 1);
-}
+       int [] ans = new int[k];
 
+       for(int x : nums){
+        map.put(x,map.getOrDefault(x,0)+1);
+       }
 
-// Create buckets where index = frequency
-// Each bucket stores numbers with that frequency
-List<Integer>[] bucket = new List[nums.length + 1];
+    int count =0;
 
 
-// Put numbers into their frequency bucket
-for (int key : map.keySet()) {
+    while(count < k){
+                    int maxfreq=0;
+                    int element =0;
 
-    int freq = map.get(key);   // get frequency
+    for ( int key : map.keySet()) {
 
-    // Create list if not present
-    if (bucket[freq] == null) {
-        bucket[freq] = new ArrayList<>();
+
+            int freq = map.get(key);
+           if(freq> maxfreq){
+            maxfreq=freq;
+             element =key;
+           }
+    }
+       
+     ans[count] = element;
+       map.remove(element);
+       count++;
     }
 
-    // Add number to its bucket
-    bucket[freq].add(key);
-}
-
-
-// Array to store answer
-int[] result = new int[k];
-int index = 0;
-
-
-// Traverse from highest frequency to lowest
-for (int i = bucket.length - 1; i >= 0 && index < k; i--) {
-
-    if (bucket[i] != null) {
-
-        // Add elements from this bucket
-        for (int num : bucket[i]) {
-
-            result[index++] = num;
-
-            // Stop when k elements are collected
-            if (index == k)
-                break;
-        }
-    }
-}
-
-return result;
+    return ans;
 
     }
 }
