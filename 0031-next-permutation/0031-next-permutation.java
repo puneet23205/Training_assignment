1c802a165b55cdfccb1a8010a6eb2be515a48442
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-02-19 22:12:22 +0530

    Time: 0 ms (100%), Space: 44.9 MB (30.87%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,44 +1,42 @@
 class Solution {
 
     public void nextPermutation(int[] nums) {
+     
+     int n= nums.length;
 
-        int n = nums.length;
-        int i = n - 2;
 
-        // Step 1: Find break point
-        while(i >= 0 && nums[i] >= nums[i+1]){
-            i--;
-        }
 
-        // Step 2: If break exists
-        if(i >= 0){
+     int i= n-2;
 
-            int j = n - 1;
+     while(i>=0 && nums[i]>=nums[i+1]){
+        i--;
+       
+     }
 
-            while(nums[j] <= nums[i]){
-                j--;
+     if( i>=0){
+
+        for(int j =n-1;j>i;j--){
+            if(nums[j]>nums[i]){
+                        int temp=nums[i];
+                         nums[i]=nums[j];
+                         nums[j]=temp;
+                         break;
             }
-
-            // swap
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
         }
+     }
 
-        // Step 3: Reverse remaining part
-        reverse(nums, i+1, n-1);
+    reverse(nums,i+1,n-1);
     }
 
-    private void reverse(int[] nums, int l, int r){
+     private void reverse(int [] nums,int left,int right){
+        while(left<right){
+            int temp=nums[left];
+            nums[left]= nums[right];
+            nums[right]=temp;
 
-        while(l < r){
-
-            int temp = nums[l];
-            nums[l] = nums[r];
-            nums[r] = temp;
-
-            l++;
-            r--;
+            left++;
+            right--;
         }
-    }
+     }
+        
 }
