3556c4593e90b7273951fb4d7d170c1b1b503961
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-03-20 19:28:14 +0530

    Time: 0 ms (100%), Space: 44.5 MB (19.87%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -11,7 +11,7 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
-        if(head == null || head.next == null) return head;
+        if(head == null || head.next == null || k==0) return head;
 
         ListNode temp = head;
         int len = 1;
@@ -22,7 +22,6 @@ class Solution {
         }
 
         k = k % len;
-        if(k == 0) return head;
 
         temp.next = head;   // make circular
 
