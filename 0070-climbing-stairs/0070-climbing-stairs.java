ee55f8f47987b94170326ec7183aed1bdd3efd3a
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-03-25 01:58:20 +0530

    Time: 0 ms (100%), Space: 42.3 MB (28.19%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,46 @@
 class Solution {
     public int climbStairs(int n) {
         
-        if(n<= 1) return n;
+         
+   // Memoization method
+       
+    //    int [] dp = new int[n+1];
+    //    Arrays.fill(dp,-1);
 
-        int prev2=1;
-        int prev1=1;
-        int curr=-1;
+    //    if(n<=1) return 1;
 
-        for(int i=2;i<=n;i++){
-            curr =prev2+ prev1;
-            prev2=prev1;
-            prev1=curr;
+    //    if(dp[n] != -1) return dp[n];
+
+    //    return climbStairs(n-1,dp) + climbStairs(n-2,dp);
+
+    // Tabulation method 
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
+        return dp[n];
 
-        return prev1;
+
+
+        // Tabulation + space optimization
+         
+        // if(n<= 1) return n;
+
+        // int prev2=1;
+        // int prev1=1;
+        // int curr=-1;
+
+        // for(int i=2;i<=n;i++){
+        //     curr =prev2+ prev1;
+        //     prev2=prev1;
+        //     prev1=curr;
+        // }
+
+        // return prev1;
+
     }
 }
