c2de4ef72e37978a6e0202029656e47672fc165f
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-03-24 23:00:27 +0530

    Time: 0 ms (100%), Space: 42 MB (80.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,31 @@
 class Solution {
     public int fib(int n) {
          
-          int [] dp= new int[n+1];
-          for(int i=0;i<=n;i++) {
-            dp[i]=-1;
-          } 
-          if(n<=1) return n;
-          dp[0]=0;
-          dp[1]=1;
+        //   int [] dp= new int[n+1];
+        //   for(int i=0;i<=n;i++) {
+        //     dp[i]=-1;
+        //   } 
+        //   if(n<=1) return n;
+        //   dp[0]=0;
+        //   dp[1]=1;
         
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+        // for(int i=2;i<=n;i++){
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
 
-        return dp[n];
+        // return dp[n];
+
+        if(n<=1) return n;
+         int prev2=0;
+         int prev=1;
+
+         int curr=-1;
+         for(int i=2;i<= n;i++){
+            curr=prev2+prev;
+            prev2=prev;
+            prev=curr;
+         }
+
+         return prev;
     }
 }
