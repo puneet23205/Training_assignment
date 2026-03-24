1b0b8b5a55ab4473464983626b0763bdd87af416
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-03-25 01:41:09 +0530

    Time: 0 ms (100%), Space: 42.3 MB (28.19%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n<= 1) return n;
+
+        int prev2=1;
+        int prev1=1;
+        int curr=-1;
+
+        for(int i=2;i<=n;i++){
+            curr =prev2+ prev1;
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        return prev1;
+    }
+}
