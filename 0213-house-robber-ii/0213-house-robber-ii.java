94002735e83c262b633e23b2c5cb28fe969291fa
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-03-26 03:36:42 +0530

    Time: 0 ms (100%), Space: 42.8 MB (72.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,19 +1,24 @@
 class Solution {
-    public int robHouse(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        if(n==1) return nums[0];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+    public int robHouse(int n,int[] nums) {
 
-        for(int i=2;i<n;i++){
-            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        int prev1=nums[0];
+        int prev2=0;
+    
+        for(int i=1;i<n;i++){
+           int pick = nums[i] + prev2;
+
+           int notpick=prev1;
+           int curr= Math.max(pick,notpick);
+
+           prev2=prev1;
+           prev1=curr;
+
         }
-        return dp[n-1];
+        return prev1;
     }
     public int rob(int[] nums) {
         int n  = nums.length;
-        if(n<2) return nums[0];
+        if(n==1) return nums[0];
         int [] FirstHouse_Skip = new int[n-1];
         int [] LastHouse_Skip = new int[n-1];
 
@@ -22,9 +27,9 @@ class Solution {
             LastHouse_Skip[i] = nums[i];
         }
 
-        int firstLoot = robHouse(FirstHouse_Skip);
+        int firstLoot = robHouse(n-1,FirstHouse_Skip);
 
-        int LastLoot = robHouse(LastHouse_Skip);
+        int LastLoot = robHouse(n-1,LastHouse_Skip);
 
         return Math.max(firstLoot,LastLoot);
 
