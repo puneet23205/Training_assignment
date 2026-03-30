55a39f51568200080470d755ddfca22ac25dc1f0
Puneet Kumar <pandey.puneet0707@gmail.com>
2026-03-31 00:30:34 +0530

    Time: 1 ms (18.6%), Space: 48.3 MB (41.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,31 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
-        return new int[]{first, last};
-    }
+        
+        List<Integer> list=new ArrayList<>();
 
-    private int findFirst(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        int first = -1;
+        for(int i=0;i<nums.length;i++){
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                first = mid;
-                right = mid - 1; // continue searching left
-            } 
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            } 
-            else {
-                right = mid - 1;
+            if(nums[i]== target){
+                list.add(i);
+                break;
             }
         }
-        return first;
-    }
 
-    private int findLast(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        int last = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                last = mid;
-                left = mid + 1; // continue searching right
-            } 
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            } 
-            else {
-                right = mid - 1;
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]==target){
+                list.add(i);
+                break;
             }
         }
-        return last;
+
+        int[] res={-1,-1};
+        int i=0;
+        for(int x: list){
+            res[i]=x;
+            i++;
+        }
+    //  if(res.length != 0)
+        return res;
+        // else return -1;
     }
 }
